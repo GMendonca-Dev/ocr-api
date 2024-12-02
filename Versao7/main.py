39e4142825bd4f3d1edef7a18d87018eef31287b
Gustavo Mendonça <153864687+gmendonca-czs@users.noreply.github.com>
2024-12-02 09:55:58 -0300

    Arquivos upados

--- Versao7/main.py
+++ Versao7/main.py
@@ -22,8 +22,8 @@ sys.path.insert(0, './Versao7')
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 warnings.simplefilter("ignore", UserWarning)
 
-START_PAGE = 221      # Número da página inicial
-END_PAGE = 300         # Número da página final
+START_PAGE = 301      # Número da página inicial
+END_PAGE = 400         # Número da página final
 DOCUMENT_ID = None  # ID do documento a ser processado (coloque o ID ou None) "93727"
 
 MAX_PAGES = 100  # Define o número máximo de páginas a serem processadas
